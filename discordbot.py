7c0e705ba3aef2ce24c6fa6f1657fc4da2a9b4ad
krsw1234krsw <67500869+haru720@users.noreply.github.com>
2020-06-28 18:14:37 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -4,7 +4,7 @@ import traceback
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
-
+client = discord.Client()
 
 @bot.event
 async def on_command_error(ctx, error):
@@ -12,7 +12,14 @@ async def on_command_error(ctx, error):
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
-@bot.command()
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+@client.event
 async def on_message(message):
     # 「唐澤貴洋」で始まるか調べる
     if message.content.startswith("唐澤貴洋"):
