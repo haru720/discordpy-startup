d11a459a8e8a5c925b84c307a7cadfdd5f012c42
krsw1234krsw <67500869+haru720@users.noreply.github.com>
2020-06-28 16:48:20 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,42 +1,21 @@
-import discord
+from discord.ext import commands
+import os
+import traceback
 
-client = discord.Client()
+bot = commands.Bot(command_prefix='/')
+token = os.environ['DISCORD_BOT_TOKEN']
 
-@client.event
-async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('------')
 
-@client.event
-async def on_message(message):
-    # 「唐澤貴洋」で始まるか調べる
-    if message.content.startswith("唐澤貴洋"):
-        # 送り主がBotだった場合反応したくないので
-        if client.user != message.author:
-            # メッセージを書きます
-            m = message.author.name + "ナイフで滅多刺しにして殺す"
-            # メッセージが送られてきたチャンネルへメッセージを送ります
-            await message.channel.send(m)
+@bot.event
+async def on_command_error(ctx, error):
+    orig_error = getattr(error, "original", error)
+    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
+    await ctx.send(error_msg)
 
-    if message.content.startswith("てすや"):
-        if client.user != message.author:
-            m ="あああああああああああああああああああああああああああああああ！！！！！！！！！！！（ﾌﾞﾘﾌﾞﾘﾌﾞﾘﾌﾞﾘｭﾘｭﾘｭﾘｭﾘｭﾘｭ！！！！！！ﾌﾞﾂﾁﾁﾌﾞﾌﾞﾌﾞﾁﾁﾁﾁﾌﾞﾘﾘｲﾘﾌﾞﾌﾞﾌﾞﾌﾞｩｩｩｩｯｯｯ！！！！！！！ ）"
-            await message.channel.send(m)
 
-    # 「プロシュート」で始まるか調べる
-    if message.content.startswith("プロシュート"):
-        # 送り主がBotだった場合反応したくないので
-        if client.user != message.author:
-            # メッセージを書きます
-            m = "神奈川県藤沢市辻堂元町2－19－19、稲村純一、葛飾商業高校中退"
-            # メッセージが送られてきたチャンネルへメッセージを送ります
-            await message.channel.send(m)
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
 
-    if message.content.startswith("あゆてやん"):
-        if client.user != message.author:
-            m ="岐阜県大垣市笠縫町387-2、安藤あゆみ"
-            await message.channel.send(m)
 
-client.run("NzI2MzY4MTM5NTQ5NDc0ODE2.XvcS2w.j2rsJRiCNWPLP8bm1P_hnkbLlmk")
+bot.run(token)
