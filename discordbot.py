963898cdb9dab23e91b21858eb5ebe7c36162ec6
krsw1234krsw <67500869+haru720@users.noreply.github.com>
2020-06-28 18:08:32 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -16,8 +16,35 @@ async def on_command_error(ctx, error):
 async def ping(ctx):
     await ctx.send('pong')
     
-async def ”唐澤貴洋”(ctx):
-    await ctx.send(message.author.name + "ナイフで滅多刺しにして殺す")  
+@client.event
+async def on_message(message):
+    # 「唐澤貴洋」で始まるか調べる
+    if message.content.startswith("唐澤貴洋"):
+        # 送り主がBotだった場合反応したくないので
+        if client.user != message.author:
+            # メッセージを書きます
+            m = message.author.name + "ナイフで滅多刺しにして殺す"
+            # メッセージが送られてきたチャンネルへメッセージを送ります
+            await message.channel.send(m)
+
+    if message.content.startswith("てすや"):
+        if client.user != message.author:
+            m ="あああああああああああああああああああああああああああああああ！！！！！！！！！！！（ﾌﾞﾘﾌﾞﾘﾌﾞﾘﾌﾞﾘｭﾘｭﾘｭﾘｭﾘｭﾘｭ！！！！！！ﾌﾞﾂﾁﾁﾌﾞﾌﾞﾌﾞﾁﾁﾁﾁﾌﾞﾘﾘｲﾘﾌﾞﾌﾞﾌﾞﾌﾞｩｩｩｩｯｯｯ！！！！！！！ ）"
+            await message.channel.send(m)
+
+    # 「プロシュート」で始まるか調べる
+    if message.content.startswith("プロシュート"):
+        # 送り主がBotだった場合反応したくないので
+        if client.user != message.author:
+            # メッセージを書きます
+            m = "神奈川県藤沢市辻堂元町2－19－19、稲村純一、葛飾商業高校中退"
+            # メッセージが送られてきたチャンネルへメッセージを送ります
+            await message.channel.send(m)
+
+    if message.content.startswith("あゆてやん"):
+        if client.user != message.author:
+            m ="岐阜県大垣市笠縫町387-2、安藤あゆみ"
+            await message.channel.send(m)
 
 
 bot.run(token)
