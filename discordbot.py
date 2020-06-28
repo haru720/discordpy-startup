8e8cd9e101561c49ab115d70562fb5adb2dfefdb
krsw1234krsw <67500869+haru720@users.noreply.github.com>
2020-06-28 18:39:49 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -18,8 +18,26 @@ async def ping(ctx):
     await ctx.send('pong')
 
 @bot.command()
-async def 唐澤貴洋(ctx):
-    await ctx.send('ナイフで滅多刺しにして殺す')
+async def 椎名(ctx):
+    await ctx.send('福島県在住、武田彪、松韻学園福島高校、ホモビ男優')
+
+@bot.command()
+async def あゆてやん(ctx):
+    await ctx.send('岐阜県大垣市笠縫町387-2、安藤あゆみ')
+    
+@bot.command()
+async def てすや(ctx):
+    await ctx.send('て、てすやっ！？')
+
+@bot.command()
+async def 脱糞(ctx):
+    await ctx.send('あああああああああああああああああああああああああああああああ！！！！！！！！！！！（ﾌﾞﾘﾌﾞﾘﾌﾞﾘﾌﾞﾘｭﾘｭﾘｭﾘｭﾘｭﾘｭ！！！！！！ﾌﾞﾂﾁﾁﾌﾞﾌﾞﾌﾞﾁﾁﾁﾁﾌﾞﾘﾘｲﾘﾌﾞﾌﾞﾌﾞﾌﾞｩｩｩｩｯｯｯ！！！！！！！)')
+
+@bot.command()
+async def プロシュート(ctx):
+    await ctx.send('神奈川県藤沢市辻堂元町2－19－19、稲村純一、葛飾商業高校中退')
+    
+
 
 bot.run(token)
 
