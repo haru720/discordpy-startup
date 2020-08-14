301190a5c1d808d44a9f129f61d5a11698c738c0
krsw1234krsw <67500869+haru720@users.noreply.github.com>
2020-08-14 22:49:32 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -14,14 +14,14 @@ async def on_ready():
     print('ログインしました')
 
 # メッセージ受信時に動作する処理
-
+@client.event
 async def on_message(message):
     # メッセージ送信者がBotだった場合は無視する
     if message.author.bot:
         return
     if message.content == 'てすや':
         await message.channel.send('当職は今日も元気ですを')
-
+@client.event
 CHANNEL_ID = 743788202543808583
 
 # 任意のチャンネルで挨拶する非同期関数を定義
@@ -32,7 +32,7 @@ async def greet():
 
 async def on_ready():
     await greet() # 挨拶する非同期関数を実行
-
+@client.event
 async def on_message(message):
     if message.content == '/削除':
         if message.author.guild_permissions.administrator:
