f63843a431c8cb431c4ad4cfb27bb51e065a1731
krsw1234krsw <67500869+haru720@users.noreply.github.com>
2020-08-14 22:53:50 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,8 +1,11 @@
+from discord.ext import commands
+import os
+import traceback
 # インストールした discord.py を読み込む
 import discord
 
 # 自分のBotのアクセストークンに置き換えてください
-TOKEN = 'DISCORD_BOT_TOKEN'
+TOKEN = os.environ['DISCORD_BOT_TOKEN']
 
 # 接続に必要なオブジェクトを生成
 client = discord.Client()
@@ -21,7 +24,7 @@ async def on_message(message):
         return
     if message.content == 'てすや':
         await message.channel.send('当職は今日も元気ですを')
-@client.event
+
 CHANNEL_ID = 743788202543808583
 
 # 任意のチャンネルで挨拶する非同期関数を定義
@@ -29,12 +32,14 @@ async def greet():
     channel = client.get_channel(CHANNEL_ID)
     await channel.send('起動')
 
-
+# bot起動時に実行されるイベントハンドラを定義
+@client.event
 async def on_ready():
     await greet() # 挨拶する非同期関数を実行
+
 @client.event
 async def on_message(message):
-    if message.content == '/削除':
+    if message.content == '/cleanup':
         if message.author.guild_permissions.administrator:
             await message.channel.purge()
             await message.channel.send('ポアします')
